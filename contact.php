d92f4df99f45129c390242605e8778cd9eb07997
MarekBeers <302248598@student.rocmondriaan.nl>
2019-09-23 08:37:14 +0200

    Rode acentjes voor de team kleur

--- contact.php
+++ contact.php
@@ -11,64 +11,65 @@
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-<div class="container">
-    <div class="jumbotron text-center" ">
-    <div class="row">
-        <div class="col-sm-3">
-            <img class="d-none d-sm-block img-fluid" src="img/zilverenkruis.png" alt="Logo">
-            <img class="d-block d-sm-none img-fluid" src="img/placeholder.png" alt="Logo">
+    <div class="container">
+        <div class="jumbotron text-center">
+        <div class="row">
+            <div class="col-sm-3">
+                <img class="d-none d-sm-block img-fluid" src="img/healthtwo_text_transparent.png" alt="Logo">
+                <img class="d-block d-sm-none img-fluid" src="img/placeholder.png" alt="Logo">
+            </div>
         </div>
     </div>
-</div>
-<nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top">
-    <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="index.php">
-        <img class="navbrand" src="img/zilverenkruis.png" alt="Logo">
-    </a>
-    <div class="collapse navbar-collapse" id="collapse_target">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
+    <nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top">
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">
+            <img class="navbrand" src="img/healthtwo_logo_transparent.png" alt="Logo">
+        </a>
+        <div class="collapse navbar-collapse" id="collapse_target">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
 
-            <li class="nav-item">
-                <a class="nav-link" href="#">Inloggen</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="contact.php">Contact</a>
-            </li>
-        </ul>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inloggen</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link text-danger" href="contact.php">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="col-sm-4">
+            <h3 class="text-center text-danger">Bel</h3>
+            <p class="p">Persoonlijk contact met een medewerker. </p>
+            <p>Voor al uw vragen.</p>
+            <h4 class="text-danger">071 751 00 52</h4>
+            <p class="p">Open vanaf <em class="text-danger">08.00 tot 20.00</em></p>
+        </div>
+        <div class="col-sm-4">
+            <h3 class="text-center text-danger">E-mail</h3>
+            <p class="p">Wanneer u geen haast heeft kunt uw ook op uw gemak een email schrijven.</p>
+            <h4 class="text-danger">zilveren@kruis.nl</h4>
+            <p class="p">Antwoord binnen 3 werkdagen</p>
+        </div>
+        <div class="col-sm-4">
+            <h3 class="text-center text-danger">Post</h3>
+            <p class="p">Wanneer u een brief wilt sturen. Vergeet de postzegel niet.</p>
+            <h5 class="text-danger">Klantenservice Zilveren Kruis </h5>
+            <h5 class="text-danger">Postbus 444</h5>
+            <h5 class="text-danger">2300 AK Leiden</h5>
+        </div>
     </div>
-</nav>
-<div class="row">
-    <div class="col-sm-4">
-        <h3 class="text-center">Bel</h3>
-        <p class="p">Persoonlijk contact met een medewerker. </p>
-        <p>Voor al uw vragen.</p>
-        <h4>071 751 00 52</h4>
-        <p class="p">Open vanaf 08.00 tot 20.00</p>
-    </div>
-    <div class="col-sm-4">
-        <h3 class="text-center">E-mail</h3>
-        <p class="p">Wanneer u geen haast heeft kunt uw ook op uw gemak een email schrijven.</p>
-        <h4>zilveren@kruis.nl</h4>
-        <p class="p">Antwoord binnen 3 werkdagen</p>
-    </div>
-    <div class="col-sm-4">
-        <h3 class="text-center">Post</h3>
-        <p class="p">Wanneer u een brief wilt sturen. Vergeet de postzegel niet.</p>
-        <h5>Klantenservice Zilveren Kruis </h5>
-        <h5>Postbus 444</h5>
-        <h5>2300 AK Leiden</h5>
-    </div>
-</div>
-<footer id="sticky-footer" class="py-4 bg-light text-dark-50">
-    <div class="container text-center">
-        <small>Copyright &copy; Zilverenkruis</small>
-    </div>
-</footer>
-</div>
+
+
+    <footer class="py-4 bg-light text-dark-50">
+        <div class="container text-center">
+            <small>Copyright <em class="text-danger"> &copy; </em>Zilverenkruis</small>
+        </div>
+    </footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <div class="jumbotron text-center" ">
             <div class="row">
                 <div class="col-sm-3">
-                    <img class="d-none d-sm-block img-fluid" src="img/zilverenkruis.png" alt="Logo">
+                    <img class="d-none d-sm-block img-fluid" src="img/healthtwo_text_transparent.png" alt="Logo">
                     <img class="d-block d-sm-none img-fluid" src="img/placeholder.png" alt="Logo">
                 </div>
             </div>
@@ -25,12 +25,12 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <a class="navbar-brand" href="index.php">
-                <img class="navbrand" src="img/zilverenkruis.png" alt="Logo">
+                <img class="navbrand" src="img/healthtwo_logo_transparent.png" alt="Logo">
             </a>
             <div class="collapse navbar-collapse" id="collapse_target">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home</a>
+                        <a class="nav-link text-danger" href="index.php">Home</a>
                     </li>
 
                     <li class="nav-item">
@@ -43,33 +43,38 @@
             </div>
         </nav>
         <div class="row text-center">
-
             <div class="col-sm-4">
                 <a href="contact.php">
                     <img class="img-fluid" src="img/doctor.png">
-                    <h3>Doctor</h3>
+                    <h3 class="text-danger">Doctor</h3>
                     <p>De docter kan uw gegevens inzien en aan passen en toevoegen.</p>
                 </a>
             </div>
-
             <div class="col-sm-4">
                 <a href="contact.php">
                     <img class="img-fluid" src="img/apotheker.png">
-                    <h3>Apotheker</h3>
+                    <h3 class="text-danger">Apotheker</h3>
                     <p>Kan recepten inzien die de doctor heeft voorgeschreven.</p>
                 </a>
             </div>
             <div class="col-sm-4">
                 <a href="contact.php">
                     <img class="img-fluid" src="img/verzekeraar.png">
-                    <h3>Zorgverzekeraar</h3>
+                    <h3 class="text-danger">Zorgverzekeraar</h3>
                     <p>Kan alles in zien voor het verzekeren voor de bedragen.</p>
                 </a>
             </div>
         </div>
-        <footer id="sticky-footer" class="py-4 bg-light text-dark-50">
+    <div class="row">
+        <div class="text-center col-sm-12">
+            <H4 class="text-danger">HealthOne</H4>
+            <p>Recepten op papier mee geven en wachten bij de apotheker is verleden tijd!</p>
+            <p>Met HealthOne wordt dit allemaal gedigitaliseerd.</p>
+        </div>
+    </div>
+        <footer class="py-4 bg-light text-dark-50">
             <div class="container text-center">
-                <small>Copyright &copy; Zilverenkruis</small>
+                <small>Copyright <em  class="text-danger"> &copy; </em>Zilverenkruis</small>
             </div>
         </footer>
     </div>
